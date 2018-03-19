9d2fda7d29f56a5ad281c3560ddb3e8a3f6c7aab
akgolcukcan <akgolcuk.can@gmail.com>
2018-03-19 22:44:58 +0300

    canakgölcük

--- helloworldapp/src/Main.java
+++ helloworldapp/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String args[]){
         System.out.println("Hello world!");
-        System.out.println("Hello world v2");
+        System.out.println("can akgölcük");
     }
 }
